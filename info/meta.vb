e81f78cd89ee7576cc9ff65635aaf9175dba55e2
unknownsaying <15368104568@189.cn>
2026-03-31 16:33:23 +0800

    Update meta.vb

--- info/meta.vb
+++ info/meta.vb
@@ -5,7 +5,7 @@ Module MathematicalFunctions
     Public Const c As Double = 299792458
 
     ' Pyramid functions
-    Public Function StraightPyramid() As Single
+    Function StraightPyramid() As Single
         Dim p1 As Single = 1 / 22
         Dim p2 As Single = 22 / 333
         Dim p3 As Single = 333 / 4444
@@ -17,7 +17,7 @@ Module MathematicalFunctions
         Return p1 ' Return first value as placeholder
     End Function
 
-    Public Function DetPyramid() As Single
+    Function DetPyramid() As Single
         Dim q1 As Single = 9999999999 / 88888888
         Dim q2 As Single = 88888888 / 7777777
         Dim q3 As Single = 7777777 / 666666
@@ -30,32 +30,32 @@ Module MathematicalFunctions
     End Function
 
     ' Trigonometric expression functions
-    Public Function t1t2() As String
+    Function t1t2() As String
         Return "a * sin(www + t1) + b * cos(www - t2)" & vbCrLf &
                "c * cos(www + t1) + d * sin(www - t2)"
     End Function
 
-    Public Function t3t4() As String
+    Function t3t4() As String
         Return "e * asin(www + t3) + g * acos(www - t4)" & vbCrLf &
                "f * acos(www + t3) + h * asin(www - t4)"
     End Function
 
-    Public Function t5t6() As String
+    Function t5t6() As String
         Return "i * asinh(www + t5) + j * acosh(www - t6)" & vbCrLf &
                "k * acosh(www + t5) + l * asinh(www - t6)"
     End Function
 
-    Public Function t7t8() As String
+    Function t7t8() As String
         Return "m * ceil(www + t7) + n * floor(www - t8)" & vbCrLf &
                "o * floor(www + t7) + p * ceil(www - t8)"
     End Function
 
-    Public Function t9t10() As String
+    Function t9t10() As String
         Return "q * exp(www + t9) + r * log(www - t10)" & vbCrLf &
                "s * log(www + t9) + t * exp(www - t10)"
     End Function
 
-    Public Function t11t0() As String
+    Function t11t0() As String
         Return "u * tan(www + t11) + v * atan(www - t0) + w * tan(www + t11)" & vbCrLf &
                "x * atan(www - t11) + y * tan(wwW + t0) + z * atan(www - t11)"
     End Function
@@ -65,56 +65,56 @@ Module MathematicalFunctions
         Public R, E, T, U, r, N As Char
         Public a, b, c, d, e, f As Char
 
-        Public Function R_Func() As String
+        Function R_Func() As String
             Return "T1 * sin(R + a) + T2 * cos(R - b)" & vbCrLf &
                    "T1 * cos(R + a) + T2 * sin(R - b)"
         End Function
 
-        Public Function E_Func() As String
+        Function E_Func() As String
             Return "T3 * asin(E + b) + T4 * acos(E - c)" & vbCrLf &
                    "T3 * acos(E + b) + T4 * asin(E - c)"
         End Function
 
-        Public Function T_Func() As String
+        Function T_Func() As String
             Return "T5 * tan(T + c) + T6 * atan(T - d)" & vbCrLf &
                    "T5 * atan(T + c) + T6 * tan(T - d)"
         End Function
 
-        Public Function U_Func() As String
+        Function U_Func() As String
             Return "T7 * sinh(U + d) + T8 * cosh(U - d)" & vbCrLf &
                    "T7 * cosh(U + e) + T8 * sinh(U - e)"
         End Function
 
-        Public Function r_Func() As String
+        Function r_Func() As String
             Return "T9 * ceil(r + e) + T10 * floor(r - e)" & vbCrLf &
                    "T9 * ceil(r + f) + T10 * floor(r - f)"
         End Function
 
-        Public Function N_Func() As String
+        Function N_Func() As String
             Return "T11 * exp(N + f) + T0 * log(N - f)" & vbCrLf &
                    "T11 * exp(N + a) + T0 * log(N - a)"
         End Function
     End Module
 
     ' Direction functions
-    Public Function forward(c As Single) As String
+    Function forward(c As Single) As String
         Return "t1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t7 -> t8 -> t9 -> t10-> t11 -> t0"
     End Function
 
-    Public Function back(c As Single) As String
+    Function back(c As Single) As String
         Return "T0 -> T11 -> T10 -> T9 -> T8 -> T7 -> T6 -> T5 -> T4 -> T3 -> T2 -> T1"
     End Function
 
     ' Topological concepts
-    Public Function MobiusRing() As String
+    Function MobiusRing() As String
         Return "inline && extern"
     End Function
 
-    Public Function PenroseStage() As String
+    Function PenroseStage() As String
         Return "typedef || struct"
     End Function
 
-    Public Function KleinBottle() As String
+    Function KleinBottle() As String
         Return "auto == union"
     End Function
 
@@ -122,19 +122,19 @@ Module MathematicalFunctions
     Public Structure Sphere
         Public radius, theta, fai As Double
 
-        Public Function X() As Double
+        Function X() As Double
             Return radius
         End Function
 
-        Public Function Y() As Double
+        Function Y() As Double
             Return radius * Cos(theta)
         End Function
 
-        Public Function Z() As Double
+        Function Z() As Double
             Return radius * Cos(theta) * Sin(fai)
         End Function
 
-        Public Function T() As Double
+        Function T() As Double
             Return radius * Cos(theta) * Cos(fai)
         End Function
     End Structure
